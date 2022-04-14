eb32a3e9b1101693b170068b891cbc5ae4accf20
Raaaaaid <52356233+Raaaaaid@users.noreply.github.com>
2022-04-14 19:51:13 +0200

    new config class

---
+++ src/config.py
@@ -0,0 +1,51 @@
+import configparser
+import os
+
+
+class Config:
+
+    """
+    This class stores all the values from the specified config_file and validates them. Values are retrieved
+    with a function call on an instance.
+    """
+
+    config_file = 'config.ini'
+
+    def __init__(self):
+
+        self._data = {}
+
+        try:
+            assert os.path.exists(self.config_file)
+        except AssertionError:
+            raise FileNotFoundError(f'{self.config_file} was not found in '
+                                    f'{os.path.dirname(os.path.realpath(__file__))}.')
+        else:
+            config = configparser.ConfigParser()
+            config.read(self.config_file)
+
+        for section in config.sections():
+            self._data[section] = {}
+            for key, val in config.items(section):
+                self._data[section][key] = val
+
+        # validate some important values
+        try:
+            assert self._data['Settings']['uplay_name']
+        except AssertionError:
+            raise ValueError('uplay_name is empty.')
+
+        try:
+            assert os.path.exists(self._data['Settings']['replay_path'])
+        except AssertionError:
+            raise ValueError('replay_path does not exist.')
+
+        if map_packs := self._data['Settings']['map_packs']:
+            map_packs = [map_pack.strip() for map_pack in map_packs.split(',')]
+            self._data['Settings']['map_packs'] = map_packs
+        else:
+            # TODO: if map_packs is empty get all map pack names from the google spreadsheet
+            pass
+
+    def __call__(self, section, key):
+        return self._data[section][key]
