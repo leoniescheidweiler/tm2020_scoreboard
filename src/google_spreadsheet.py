0cf05dd89ae9685672ec2289110b2d50c0f5ddc3
Raaaaaid <52356233+Raaaaaid@users.noreply.github.com>
2022-04-16 17:54:46 +0200

    finished replay synchronization

--- src/google_spreadsheet.py
+++ src/google_spreadsheet.py
@@ -1,4 +1,5 @@
 import gspread
+from gspread.utils import rowcol_to_a1
 
 from config import Config
 from replay import Replay
@@ -9,6 +10,7 @@ class GoogleSpreadsheet:
     no_map_pack_worksheets = ['Season Overview', 'Template']
 
     # TODO: create Template automatically so that these hardcoded values can be specified there?
+    # TODO: pass Template instance to synchronize_replay_times()?
     track_name_col = 5
     header_rows = 2
 
@@ -43,28 +45,42 @@ class GoogleSpreadsheet:
         Synchronizes all the replay times for all map packs listed in config.ini. If there are not any map packs
         listed, then all map packs with an existing worksheet in the spreadsheet will be updated.
         """
+        print(f'Hello {self.config("Settings", "uplay_name")}.')
+
         if map_pack_titles := self.config('Settings', 'map_packs'):
             worksheets = [self.open_worksheet(title) for title in map_pack_titles]
         else:
             worksheets = self.worksheets
 
+        new_records_total = 0
         for worksheet in worksheets:
             track_names = worksheet.col_values(self.track_name_col)[self.header_rows:]
             player_col = worksheet.find(self.config('Settings', 'uplay_name')).col
-            old_times_s = worksheet.col_values(player_col)[self.header_rows:]
-            if (diff := (len(track_names) - len(old_times_s))) > 0:
-                old_times_s += [''] * diff
-            old_times_s = [float(time) if time else float('inf') for time in old_times_s]
+            times_s = worksheet.col_values(player_col)[self.header_rows:]
+            # pad times_s so that it has the same length as track_names
+            if (diff := (len(track_names) - len(times_s))) > 0:
+                times_s += [''] * diff
+            # empty cells get an infinite time
+            times_s = [float(time) if time else float('inf') for time in times_s]
+            new_records_map_pack = 0
             for i, track_name in enumerate(track_names):
                 try:
                     track_time_s = Replay(track_name).time_s
                 except FileNotFoundError:
                     continue
                 else:
-                    old_time_s = old_times_s[i]
-                    if old_time_s > track_time_s:
-                        # TODO: update cell values with batch update
-                        pass
+                    if times_s[i] > track_time_s:
+                        times_s[i] = track_time_s
+                        new_records_map_pack += 1
+            # replace inf with empty strings and reformat list
+            times_s = [[time] if time != float('inf') else [''] for time in times_s]
+            # batch update new times
+            start_cell_times = rowcol_to_a1(self.header_rows + 1, player_col)
+            stop_cell_times = rowcol_to_a1(self.header_rows + len(track_names), player_col)
+            worksheet.update(f'{start_cell_times}:{stop_cell_times}', times_s)
+            new_records_total += new_records_map_pack
+            print(f'{worksheet.title}: Updated {new_records_map_pack} times.')
+        print(f'Updated {new_records_total} times in total. Congratulations!')
 
 
 if __name__ == '__main__':
